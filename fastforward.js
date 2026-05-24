52fbf1411a39f19a801a61573ae73b2a2b342ef6
Noob Software <99470053+noobsoftware@users.noreply.github.com>
2026-05-24 21:01:47 +0000

    Update fastforward.js

--- fastforward.js
+++ fastforward.js
@@ -1,36 +1,41 @@
 (function() {
-	console.log('load noobjs');
-
-	window.MutationObserver = function() {
-	    this.observe = function() {};
-	    this.disconnect = function() {};
-	    this.takeRecords = function() { return []; };
-	};
-
 	setInterval(function() {
 		var targetNode = document.querySelector('.html5-main-video');
 		var targetNodeParent = document.querySelector('.html5-video-player');
-		console.log(targetNode);
 
-		if(targetNodeParent.classList.contains('ad-showing')) {
+		if(targetNodeParent.classList.contains('ad-showing') && !targetNodeParent.classList.contains('ad-interrupted')/* && document.querySelector('.ytp-skip-ad-button') == null*/) {
 			targetNode.muted = true;
 
 			targetNode.currentTime += 25;
 
-			/*document.querySelectorAll('div').forEach(div => {
+			document.querySelectorAll('.title').forEach(div => {
 				div.style.backgroundColor = 'rgba(25,0,255,0.1)';
-			});*/
+			});
 
-
+			targetNode.classList.add('ad_noob');
 		} else {
-			targetNode.muted = false;
-			/*document.querySelectorAll('div').forEach(div => {
-				div.style.backgroundColor = 'rgba(25,255,0,0.1)';
-			});*/
+			if(document.querySelector('.ytp-skip-ad-button') == null) {
+				if(targetNodeParent.classList.contains('ad_noob')) {
+					targetNode.currentTime = 0;
+					targetNodeParent.classList.remove('ad_noob');
+				}
+
+				targetNode.muted = false;
+				document.querySelectorAll('.title').forEach(div => {
+					div.style.backgroundColor = 'rgba(25,255,0,0.1)';
+				});
+			} else {
+				if(targetNode.classList.contains('ad_noob')) {
+					targetNode.currentTime = 0;
+				} else {
+					setTimeout(function() {
+						document.querySelector('.ytp-skip-ad-button').click();
+					}, 650);
+				}
+			}
 		}
 
 	}, 250);
-	console.log('insert noobjs');
 
 
 }());
